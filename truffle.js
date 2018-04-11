380972e9d345412e6d23d8a8e47b276f95ce0819
ghostbuster91 <kghost0@gmail.com>
2018-04-11 12:37:44 +0200

    Configure ropsten

--- truffle.js
+++ truffle.js
@@ -1,4 +1,18 @@
+require('dotenv').config({path: '.env.local'});
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    ropsten: {
+	    provider: function(){
+	    	var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
+			var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+	    	return new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
+	    },
+	    gas: 4600000,
+      	gasPrice: web3.utils.toWei("20", "gwei"),
+	    network_id: '3',
+	}
 };
